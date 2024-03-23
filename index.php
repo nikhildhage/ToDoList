00393eb295c92001dd70411b5636648881d49a80
nikhildhage <nikhil.dhage@gmail.com>
2024-03-23 14:07:59 -0700

    Updated index to include database file

--- index.php
+++ index.php
@@ -8,9 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>ToDoList</title>
 </head>
 <body>
+    <?php
+    include("Database.php");
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
 </html>
