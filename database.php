2efdb17b5f82bd0d489bf06b3430c4c071315d1a
nikhildhage <nikhil.dhage@gmail.com>
2024-03-23 14:08:29 -0700

    Fixed Database Connection

--- database.php
+++ database.php
@@ -1,14 +1,13 @@
 <?php
 
 // Database connection settings
-$dsn = 'mysql:localhost';
-$db_name = 'todoist';
+$dsn = "mysql:localhost;dbname='toDoList'";
 $db_user = 'root';
 $db_pass = ' ';
 
 try {
     // Create PDO connection
-    $db = new PDO($dsn, $db_name, $db_user);
+    $db = new PDO($dsn, $db_user);
     
     //Display Success Message
     $Success_message="Connected to database successfully!";
@@ -16,7 +15,7 @@ try {
 } catch (PDOException $e) {
     // Catch any errors and display
     $error_message="Database Error ";
-    $error_message.=$e->getMessage();
+    $error_message.=" " . $e->getMessage();
     echo $error_message;
     exit();
 }
